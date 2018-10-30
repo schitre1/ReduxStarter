8f03a63e6e917b72bee9665254498b1d0819b5b9
Chitre <schitre@paypal.com>
2018-10-29 22:41:08 -0700

    add conditional rendering and more props

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,12 +3,24 @@ import {connect} from 'react-redux';
 
 class BookDetail extends Component {
     render () {
+        //Application state is completely different from component state, so we can do things like -
+        //this.state.ldjfigejga
+        //this.setState
+        if(!this.props.book) {
+            //to handle when the app starts up and state is nill as specified on reducer_active_book.
+            return <div>Select a book to get started.</div>;
+        }
         return (
-            <div> Book Detail!</div>
+            <div>
+                <h3>Details for:</h3>
+                <div>Title: {this.props.book.title}</div>
+                <div>Pages: {this.props.book.pages}</div>
+            </div>
         );
     }
 }
 
+//map application state to thios components props
 function mapStateToProps(state) {
     return {
         book: state.activeBook 
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        {'title': 'JavaScript The Good Parts'},
-        {'title': 'Harry Potter'},
-        {'title': 'The Alchemist'},
-        {'title': 'Eloquent Ruby'},
+        {title: 'JavaScript The Good Parts', pages: 100},
+        {title: 'Harry Potter', pages: 300},
+        {title: 'The Alchemist', pages: 200},
+        {title: 'Eloquent Ruby', pages: 150},
     ]
 }
